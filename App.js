e56f5bac19d5f270f3561c946c35aaf929b2c94b
Exequiel.Wiedermann <ewiedermann@init-dev.com.ar>
2022-05-19 00:09:28 -0300

    Explicación de props

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginForm from './src/UI/components/LoginForm';
+import Saludar from './src/UI/components/Saludar';
 
 //Acá conecto mis pantallas
+
+//El saludar es estático ahora
+//      <Saludar/>
+//      <Saludar/>
+//      <Saludar/>
+
+//Esto es un props
+//<View style={styles.container}>
 export default function App() {
   return (
     <View style={styles.container}>
       
-      <LoginForm/>
+      <Saludar name="Exequiel Wiedermann"/>
+
     </View>
   );
 }
