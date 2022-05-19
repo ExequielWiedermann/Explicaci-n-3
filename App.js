fc7d7a586c4299af289bb0d708c72c556f1e557b
Exequiel.Wiedermann <ewiedermann@init-dev.com.ar>
2022-05-18 23:26:38 -0300

    Iniciando el proyecto

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Empezando la app</Text>
+      <Text>Hola probando</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/UI/components/LoginForm.js
@@ -0,0 +1,4 @@
+//Siempre deben de importar la estructura de react
+import react from "react";
+//Este import genera la transición a los react
+import {} from "react-native"
