102cc01f566c1721913f0aba6d187874c65e9c01
Exequiel.Wiedermann <ewiedermann@init-dev.com.ar>
2022-05-29 23:07:44 -0300

    Utilizando dos props para el proyecto

--- src/UI/components/Saludar.js
+++ src/UI/components/Saludar.js
@@ -3,12 +3,18 @@ import {Text} from "react-native"
 
 //Con los props reutilizamos codigo, con un solo componente hacemos varios saludos
 export default function Saludar(props){
-    const {name} = props;
-    console.log(name);
+
+    const{ firstname, lastname } = props;
 
     //Los props los usamos para enviar de una pantalla a otra (manejan estados!!!)
 
     //El return es jsx!!!
     //Las variables se envuelven entre {}
-    return <Text>Pesadillas {name}</Text>
+    return <Text>Pesadillas {firstname} {lastname}</Text>
 }
+
+//Para utilizar dos variables en el props se hace de la siguiente forma
+Saludar.defaultProps = {
+    firstname: "Exequiel",
+    lastname: "Wiedermann",
+};
